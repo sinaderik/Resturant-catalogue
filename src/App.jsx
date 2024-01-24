f9b316d53c2a718724862b1b7d55e02c0a4a906c
sina <sinaderik7@gmail.com>
2024-01-24 18:46:40 +0330

    feat: header component created

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './components/Header/Header'
+
+export default function App() {
+  return (
+    <div className='wrapper bg-faded-dark'>
+      <Header />
+    </div>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+export default function Header() {
+    return (
+        <header className='bg-size-cover bg-position-center py-5'>
+            <div className='container'>
+                <h1 className='text-start text-light text-uppercase'>
+                    fast food catalogue
+                </h1>
+            </div>
+        </header>
+    )
+}
